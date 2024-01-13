51aefec04581f808e261892bfa38322a864c0b87
Harsh Rohilla <rohillaharsh172k4@gmail.com>
2024-01-13 14:41:38 +0530

    Add(v-1): Login for school
    Updated the schoolName to schoolId and added login functionality for the school

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,9 +1,13 @@
 import express from "express"
-import { addSchool } from "../controllers/schoolController.js"
+import { addSchool, loginSchool } from "../controllers/schoolController.js"
 
 const router = express.Router()
 
 // Add school details
 router.post('/addschool', addSchool)
+
+// Get school details
+router.post('/loginschool', loginSchool)
+
 
 export default router
